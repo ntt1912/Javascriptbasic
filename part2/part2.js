fafe5bca0b319d3efec8b206e6e99689f4586410
ntt1912 <124802515+ntt1912@users.noreply.github.com>
2023-05-07 22:00:21 +0700

    Part4

--- part2/part2.js
+++
@@ -1,5 +0,0 @@
-console.log("Starting")
-
-let x = 5, y = 6, z = 7;
-
-console.log("Testing: ", x, y, z)
---
+++ part2/part4.js
@@ -0,0 +1,6 @@
+let a = 'tôi là string-chuỗi'
+let text = `We are the so-called "Viking" of the beach`
+
+console.log(text)
+console.log(a)
+
