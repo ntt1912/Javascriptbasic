3e8cbe369a6282b2984eb107ea4f4c0973900bc1
ntt1912 <124802515+ntt1912@users.noreply.github.com>
2023-05-08 14:36:03 +0700

    part7

--- part2/part6.js
+++
@@ -1,4 +0,0 @@
-console.log(`Hello world!`.toLocaleUpperCase())
-let a = 5;
-let b = 6.9;
-console.log(a + b)
---
+++ part2/part7.js
@@ -0,0 +1,14 @@
+console.log(`Hello world!`.toLocaleUpperCase())
+let a = 5;
+let b = 10;
+console.log(`fifteen is ${a + b} and not ${2 * a + b}`)
+let obj = {
+    name: 'Thanh',
+    address: 'Thanh Hoa',
+    email: '',
+    gender: '',
+}
+
+console.log(`my name's`, obj.name)
+console.log(`my address's`, obj.address)
+
